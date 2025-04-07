d5ef09b01d12ddb58fffa27aa03ae9e4999cce03
Sooraj <soorajraveendran@yahoo.com>
2025-04-07 12:40:36 +0530

    Remove all files and directories

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Seller)
-admin.site.register(Listing)
-admin.site.register(ListingImage)
-admin.site.register(BuyRequest)
-admin.site.register(Order)
-admin.site.register(Message)
